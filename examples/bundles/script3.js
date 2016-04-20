6205ddb42ead20c81c51ddd4a7ebcdc4d66b6e63
Danny <danny@webdevelopers.eu>
2016-04-20 22:06:37 +0200

    examples/bundles: added referenced script from within bundle

---
+++ examples/bundles/script3.js
@@ -0,0 +1,5 @@
+// This file is linked from bundle.html's #script3 as external resource.
+function Prototype3() {
+    message('Object created using function `Prototype3`');
+}
+message('Script script3.js linked from bundle.html\'s #script3 tag evaluated.');
