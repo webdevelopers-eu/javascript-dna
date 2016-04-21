e09c8ba0eea719e24657d670d32dce58fe74bf4b
Danny <danny@webdevelopers.eu>
2016-04-21 17:50:07 +0200

    Autocommit.

--- bin/minimize.sh
+++ bin/minimize.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 pushd "`dirname $0`/.."
 uglifyjs -m -c --screw-ie8 --preamble "/*! Javascript DNA v1.0 | (c) Daniel Ševčík | www.webdevelopers.eu | MIT license | https://github.com/webdevelopers-eu/javascript-dna */" --lint dna.js -o dna.min.js --source-map dna.min.map
+git commit -m "New minimized version." dna.min.*
 popd
