e4f02bea89582826e015c8789b6837f8638fbdb6
Danny <danny@webdevelopers.eu>
2016-04-25 22:09:45 +0200

    Auto-minify GIT hook implemented.

--- bin/minimize.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pushd "`dirname $0`/.."
-uglifyjs -m -c --screw-ie8 --comments --lint dna.js -o dna.min.js --source-map dna.min.map
-#git commit -m "New minimized version." dna.min.*
-popd
